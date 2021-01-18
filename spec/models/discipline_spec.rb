3b1695b927d61782784fbd96019e906cdf29c95b
Thiago Guerrero <tgbalera@hotmail.com>
2021-01-18 15:49:39 -0300

    fix lint problems at tests
    Co-authored-by: Thiago Guerrero <tgbalera@hotmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- spec/models/discipline_spec.rb
+++ spec/models/discipline_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Discipline, type: :model do
-  before do
-    @att = {
+  let(:valid_att) do
+    {
       name: 'ACH1575 - Inovação em Serviços de Lazer e Turismo',
       campus: 'USP Leste',
       unity: 'Escola de Artes, Ciências e Humanidades - EACH',
@@ -24,64 +24,59 @@ RSpec.describe Discipline, type: :model do
       },
       keywords: ['Inovação']
     }
-
   end
+
   it 'is valid with valid attributes' do
-    discipline = described_class.new(@att)
-    expect(discipline).to be_valid  
+    discipline = described_class.new(valid_att)
+    expect(discipline).to be_valid
   end
 
-  it "is invalid with a invalid name" do
-    attr = @att
-    attr['name'] = "Foo"
+  it 'is invalid with a invalid name' do
+    attr = valid_att
+    attr[:name] = 'Foo'
     discipline = described_class.new(attr)
-    expect(discipline).to be_invalid 
-  end
-
-  it 'is invalid with a invalid URL' do
-    attr = @att
-    attr['url'] = "Foo"
-    discipline = described_class.new(attr)
-    expect(discipline).to be_invalid 
+    expect(discipline).to be_invalid
   end
 
   it 'is invalid with a invalid URL' do
-    attr = @att
-    attr['url'] = "Foo"
+    attr = valid_att
+    attr[:url] = 'Foo'
     discipline = described_class.new(attr)
-    expect(discipline).to be_invalid 
+    expect(discipline).to be_invalid
+  end
+
+  it 'is invalid with a invalid level' do
+    attr = valid_att
+    attr[:level] = 'Foo'
+    discipline = described_class.new(attr)
+    expect(discipline).to be_invalid
   end
 
   it 'is invalid with a invalid nature' do
-    attr = @att
-    attr['nature'] = "Foo"
+    attr = valid_att
+    attr[:nature] = 'Foo'
     discipline = described_class.new(attr)
-    expect(discipline).to be_invalid 
+    expect(discipline).to be_invalid
   end
 
   it 'is invalid with a invalid campus' do
-    attr = @att
-    attr['campus'] = "Foo"
+    attr = valid_att
+    attr[:campus] = 'Foo'
     discipline = described_class.new(attr)
-    expect(discipline).to be_invalid 
+    expect(discipline).to be_invalid
   end
 
   it 'is invalid with a invalid unity' do
-    attr = @att
-    attr['unity'] = "Foo"
+    attr = valid_att
+    attr[:unity] = 'Foo'
     discipline = described_class.new(attr)
-    expect(discipline).to be_invalid 
+    expect(discipline).to be_invalid
   end
 
-  it 'is invalid with a invalid category' do
-    attr = @att
-    attr['category'] = {
-      business: false,
-      entrepreneurship: false,
-      innovation: false,
-      intellectualProperty: false
-    }
+  it 'is invalid without category' do
+    attr = valid_att
+    attr[:category][:innovation] = false
     discipline = described_class.new(attr)
-    expect(discipline).to be_invalid 
+    expect(discipline).to be_invalid
   end
 end
