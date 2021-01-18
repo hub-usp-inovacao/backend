d82db73d2d223e6d5ccc75d12475f588dd066a15
Thiago Guerrero <tgbalera@hotmail.com>
2021-01-18 15:24:06 -0300

    add url validation for discipline
    Co-authored-by: Thiago Guerrero <tgbalera@hotmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Discipline
   include Mongoid::Document
 
@@ -12,13 +14,18 @@ class Discipline
   field :category, type: Hash
   field :keywords, type: Array
 
-  validates_presence_of :name, :campus, :unity, :description, :category, :nature, :level
+  validates :name, :campus, :unity, :description, :category, :nature, :level, presence: true
   validates :name, format: { with: /\A(\w|\d){3}\d{4} (-|–) .+\z/, message: '' }
-
+  validates :url, url: true
+  validate :a_valid_level?
   # nature: duas opções (graduação and pós-graduação)
-  # campus and unity: tem uma lista definida 
-  # level: tem uma lista definida (roadmap de empreendedorismo) 
+  # campus and unity: tem uma lista definida
   # url: formatar com regex
-  # category: tem que ser um hash com 4 chaves específicas, sendo pelo menos 
+  # category: tem que ser um hash com 4 chaves específicas, sendo pelo menos
   #           uma delas 'true' (business, entrepreneurship, innovation, intellectual_property)
+
+  def a_valid_level?
+    true
+    # level: tem uma lista definida (roadmap de empreendedorismo)
+  end
 end
