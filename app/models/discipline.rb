8a0ea323388d83049c394f9c94e0854f695b1b64
Thiago Guerrero <tgbalera@hotmail.com>
2021-01-18 21:49:24 -0300

    finish all validations for discipline
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require "#{Rails.root}/data/entrepreneurship_levels"
+require Rails.root.join('data/entrepreneurship_levels')
+require Rails.root.join('data/campi')
 
 class Discipline
   include Mongoid::Document
@@ -19,15 +20,33 @@ class Discipline
   validates :name, :campus, :unity, :description, :category, :nature, :level, presence: true
   validates :name, format: { with: /\A(\w|\d){3}\d{4} (-|–) .+\z/, message: '' }
   validates :url, url: true
-  validate :a_valid_level?
-  # nature: duas opções (graduação and pós-graduação)
-  # campus and unity: tem uma lista definida
-  # url: formatar com regex
-  # category: tem que ser um hash com 4 chaves específicas, sendo pelo menos
-  #           uma delas 'true' (business, entrepreneurship, innovation, intellectual_property)
+  validate :a_valid_level?, :a_valid_nature?, :a_valid_campi?, :a_valid_unity?, :a_valid_category?
 
   def a_valid_level?
     valid_levels = EntrepreneurshipLevels.new.call
     errors.add(:level, 'must be a valid level') unless valid_levels.include?(level)
+  end
+
+  def a_valid_nature?
+    valid_natures = %w[graduação pós-graduação]
+    errors.add(:nature, 'must be a valid nature') unless valid_natures.include?(nature.downcase)
+  end
+
+  def a_valid_campi?
+    valid_campi = Campi.new.call.map { |c| c[:name] }
+    errors.add(:campus, 'must be a valid campi') unless valid_campi.include?(campus)
+  end
+
+  def a_valid_unity?
+    valid_unities = Campi.new.call.reduce([]) do |acc, c|
+      acc.concat(c[:unities])
+    end
+    errors.add(:campus, 'must be a valid campi') unless valid_unities.include?(unity)
+  end
+
+  def a_valid_category?
+    unless category.any? { |_key, value| value == true }
+      errors.add(:category, 'at least one category must be true')
+    end
   end
 end
