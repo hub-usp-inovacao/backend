ed8a3ea7fd9d22119885d56c5f1657bf5038b911
Thiago Guerrero <tgbalera@hotmail.com>
2021-01-20 15:34:15 -0300

    Add list of validations for patents
    Co-authored-by: erickrodrigs erick.rodrigues.santana14@gmail.com
    Co-authored-by: João Daniel jotaf.daniel@gmail.com
    Co-authored-by: Thiago Guerrero tgbalera@hotmail.com

--- app/models/patent.rb
+++ app/models/patent.rb
@@ -3,14 +3,34 @@
 class Patent
   include Mongoid::Document
 
-  field :name, type: String
   field :classification, type: Hash
   field :ipcs, type: Array
   field :owners, type: Array
   field :inventors, type: Array
   field :countries_with_protection, type: Array
+  field :name, type: String
   field :status, type: String
   field :sumary, type: String
   field :url, type: String
   field :photo, type: String
+
+  validates :name, :classification, :ipcs, :owners, :status, presence: true
+
+  # Nome: Não pode ser vazio
+
+  # Classificação:
+  # CIP: "W - NOME"
+  # Sub-Área: "W__ - NOME"
+  # Primário obrigatória, segundário opcional
+
+  # IPCS: "X__Y______" EX: "G01R002722" 
+  
+  # Owners, Iventors: Não pode ser vazio
+
+  # Status: Lista definida(Concedida, Domínio Público e Em análise)
+
+  # url: Checar se é um link
+
+  # photo: ID Flyer
+
 end
