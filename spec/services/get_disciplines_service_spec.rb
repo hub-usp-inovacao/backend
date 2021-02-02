a165f0a6e5a8bf31ef1b898a0bd46c5d1ba61d72
Thiago Guerrero <tgbalera@hotmail.com>
2021-02-02 12:30:07 -0300

    Discipline service tests refactoring
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- spec/services/get_disciplines_service_spec.rb
+++ spec/services/get_disciplines_service_spec.rb
@@ -4,20 +4,20 @@ require 'rails_helper'
 
 RSpec.describe GetDisciplinesService, type: :model do
   describe 'GetDisciplinesService::request' do
-    before do
+    it 'handle response successfully' do
       mock_response = instance_double(RestClient::Response,
                                       body: {
                                         'isAvailable' => true,
                                         'imageAvailable' => false
                                       }.to_json)
-
       allow(RestClient::Request).to receive(:execute).and_return(mock_response)
-
-      @response = described_class.request
+      expect(described_class.request).to be_truthy
     end
 
-    it 'handle response successfully' do
-      expect(@response).to be_truthy
+    it 'return nil when response fails' do
+      allow(RestClient::Request).to receive(:execute)
+        .and_raise(RestClient::ExceptionWithResponse)
+      expect(described_class.request).to be_nil
     end
   end
 
@@ -26,12 +26,12 @@ RSpec.describe GetDisciplinesService, type: :model do
       described_class.class_variable_set :@@data, [[], []]
     end
 
-    it 'does not raise errors' do
+    it 'does not raise errors when record is successfully created' do
       allow(Discipline).to receive(:create_from).and_return(true)
       expect { described_class.parse }.not_to raise_error
     end
 
-    it 'does raise errors' do
+    it 'return nil when record creation fails' do
       allow(Discipline).to receive(:create_from)
         .and_raise(Mongoid::Errors::Validations, Discipline.new)
       expect(described_class.parse).to be_nil
