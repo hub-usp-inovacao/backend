eb7048983ac563776dbdd9a29942a05f4989979e
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-02-10 15:15:04 -0300

    mock data for discipline request tests
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- spec/requests/discipline_spec.rb
+++ spec/requests/discipline_spec.rb
@@ -3,6 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Disciplines', type: :request do
+  let(:disciplines) do
+    [
+      {
+        name: 'ACH1575 - Inovação em Serviços de Lazer e Turismo',
+        campus: 'USP Leste',
+        unity: 'Escola de Artes, Ciências e Humanidades - EACH',
+        start_date: '2020',
+        nature: 'Graduação',
+        level: 'Preciso testar minha ideia!',
+        url: 'https://uspdigital.usp.br/jupiterweb/obterDisciplina?sgldis=ACH1575',
+        description: {
+          short: 'Foo',
+          long: 'Bar'
+        },
+        category: {
+          business: false,
+          entrepreneurship: false,
+          innovation: true,
+          intellectualProperty: false
+        },
+        keywords: ['Inovação']
+      }
+    ]
+  end
+
   def discipline_keys
     %w[_id name campus unity start_date
        nature level url description
@@ -11,6 +36,7 @@ RSpec.describe 'Disciplines', type: :request do
 
   describe 'GET /index' do
     before do
+      allow(Discipline).to receive(:all).and_return(disciplines)
       get '/disciplines'
     end
 
