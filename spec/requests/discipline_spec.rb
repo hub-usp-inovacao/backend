4c0bb0f5e94722adcf41745a0267d419df700d42
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-02-10 00:17:04 -0300

    refactor discipline request tests
    add a new test to check the response's http status
    
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- spec/requests/discipline_spec.rb
+++ spec/requests/discipline_spec.rb
@@ -3,29 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Disciplines', type: :request do
-  let(:discipline) do
-    {
-      name: 'ACH1575 - Inovação em Serviços de Lazer e Turismo',
-      campus: 'USP Leste',
-      unity: 'Escola de Artes, Ciências e Humanidades - EACH',
-      start_date: '2020',
-      nature: 'Graduação',
-      level: 'Preciso testar minha ideia!',
-      url: 'https://uspdigital.usp.br/jupiterweb/obterDisciplina?sgldis=ACH1575',
-      description: {
-        short: 'Foo',
-        long: 'Bar'
-      },
-      category: {
-        business: false,
-        entrepreneurship: false,
-        innovation: true,
-        intellectualProperty: false
-      },
-      keywords: ['Inovação']
-    }
-  end
-
   def discipline_keys
     %w[_id name campus unity start_date
        nature level url description
@@ -34,12 +11,14 @@ RSpec.describe 'Disciplines', type: :request do
 
   describe 'GET /index' do
     before do
-      Discipline.delete_all
-      Discipline.create(discipline)
+      get '/disciplines'
     end
 
-    it 'returns a list of all disciplines' do
-      get '/disciplines'
+    it 'returns a response with success http status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns a list of all disciplines with expected format' do
       resp = JSON.parse(response.body)
       resp.each do |discipline|
         expect(discipline.keys.difference(discipline_keys)).to eq([])
