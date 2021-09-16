3d10ebff2b2eda9f4d0ddf161f9aa2d419315d56
João Daniel <jotaf.daniel@gmail.com>
2021-09-16 15:14:59 -0300

    add config for Rails 6 host blocking
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Thiago Guerrero <tgbalera@hotmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
+  config.hosts << 'hubuspinovacao.if.usp.br'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
