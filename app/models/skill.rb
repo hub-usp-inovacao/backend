17ba944b1b358b0b10ccdf7a2d989ecc0eb09522
João Daniel <jotaf.daniel@gmail.com>
2021-09-26 08:21:14 -0300

    add more attrs to skills
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -19,6 +19,28 @@ class Skill
   field :bond, type: String
 
   validates :name, :email, :unities, :campus, presence: true
+  validates :lattes, :picture, url: true
+
+  validate :a_valid_campi?, :a_valid_area?
+
+  def a_valid_area?
+    is_valid = !area.nil? &&
+               area.keys.sort.eql?(%w[major minor]) &&
+               knowledge_areas.any? do |karea|
+                 karea[:name].eql?(area[:major]) && karea[:subareas].include?(area[:minor])
+               end
+
+    errors.add(:area, 'invalid area') unless is_valid
+  end
+
+  def a_valid_campi?
+    is_valid = !campus.nil? &&
+               campi.any? do |valid_campus|
+                 valid_campus[:name].eql?(campus)
+               end
+
+    errors.add(:campus, 'invalid campus') unless is_valid
+  end
 
   def self.create_from(row)
     new_skill = Skill.new({
@@ -26,13 +48,51 @@ class Skill
                             email: row[3],
                             unities: row[5],
                             campus: row[6],
-                            phone: format_phone(row)
-                            # TODO: add the rest of attrs
+                            phone: format_phone(row),
+                            description: {
+                              skills: row[23],
+                              services: row[24],
+                              equipments: row[25]
+                            },
+                            area: {
+                              major: row[26],
+                              minor: row[27]
+                            },
+                            keywords: row[28].split(';'),
+                            labs_or_groups: labs_or_groups_from(row)
                           })
 
     raise StandardError, new_skill.errors.full_messages unless new_skill.save
 
     new_skill
+  end
+
+  def label_lab_or_group(name, initials, bond)
+    label = name
+
+    no_initials = initials.nil? || initials.eql?('N/D')
+    no_bond = bond.nil? || bond.eql?('N/D')
+
+    label = "#{label} (#{initials})" unless no_initials
+    label = "#{label} - #{bond}" unless no_bond
+
+    label
+  end
+
+  def self.format_lab_or_group(row)
+    {
+      name: row[2],
+      label: label_lab_or_group(row[2], row[3], row[0]),
+      site: row[4]
+    }
+  end
+
+  def self.labs_or_groups_from(row)
+    first = format_lab_or_group(row[8..12])
+    second = format_lab_or_group(row[13..17])
+    third = format_lab_or_group(row[18..22])
+
+    [] << first << second << third
   end
 
   # TODO: extract into validator
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let :valid_attr do
+    {
+      name: 'Alfredo Goldman',
+      email: 'gold@ime.usp.br',
+      unities: ['Instituto de Matemática e Estatística'],
+      campus: 'Butantã',
+      labs_or_groups: [],
+      description: {},
+      area: {},
+      phone: '(11) 3091-6497',
+      keywords: ['high performance computing', 'agile methodologies'],
+      lattes: 'http://lattes.cnpq.br/2118391660819227',
+      picture: '',
+      limit_date: '',
+      bond: 'Docente'
+    }
+  end
+
+  it 'is a placeholder' do
+    expect(1 + 1).to be(2)
+  end
 end
