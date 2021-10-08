18f074de367fc23bcb8f40e0f8a8d9b4abfda952
Thiago Guerrero <tgbalera@hotmail.com>
2021-10-08 16:12:59 -0300

    fix request params acceptance
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- app/controllers/company_updates_controller.rb
+++ app/controllers/company_updates_controller.rb
@@ -2,6 +2,7 @@
 
 class CompanyUpdatesController < ApplicationController
   def create
+    ActionController::Parameters.permit_all_parameters = true
     data = create_params
     @comp_update = CompanyUpdate.create!(data)
     render json: { company_update: @comp_update }
@@ -12,6 +13,6 @@ class CompanyUpdatesController < ApplicationController
   private
 
   def create_params
-    params.require(:company).permit(:name, :cnpj, partners_values: {}, company_values: {})
+    params.require(:company)
   end
 end
