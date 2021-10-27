a02733a60dd58c18beeb1f21c28ef158e42d18f0
Thiago Guerrero <tgbalera@hotmail.com>
2021-10-27 16:08:49 -0300

    fix prod configs
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,22 +19,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'hubusp.inovacao.usp.br',
-    user_name: ENV['mail_username'],
-    password: ENV['mail_password'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-
   config.active_support.deprecation = :log
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
