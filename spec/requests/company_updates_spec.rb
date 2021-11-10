d71074ce59735d4c1c066d957e16193a42a8ab22
João Daniel <jotaf.daniel@gmail.com>
2021-11-10 20:41:30 -0300

    add mongoid i18n fallback
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- spec/requests/company_updates_spec.rb
+++ spec/requests/company_updates_spec.rb
@@ -5,37 +5,53 @@ require 'rails_helper'
 RSpec.describe 'CompanyUpdates', type: :request do
   let(:valid_data) do
     {
-      cnpj: '1234.4321.4232/0001.21',
       name: 'fulano',
+      cnpj: '12.123.123/0001-21',
       partners_values: [
         {
           name: 'Paulo',
           phone: '11 999999999',
           email: 'Foo@bar.com',
-          bond: 'Aluno ou ex-aluno de graduação',
+          bond: 'Aluno ou ex-aluno (graduação)',
           nusp: '11111111',
-          unity: 'Escola de Comunicação e Artes - ECA'
+          unity: 'Escola de Artes, Ciências e Humanidades - EACH'
+        },
+        {
+          name: 'Paulo Outro',
+          phone: '11 999999999',
+          email: 'Foo@bar.com',
+          bond: '',
+          nusp: '',
+          unity: ''
         }
       ],
       company_values: {
-        Foo: 'Bar'
+        CEP: '05331-020'
+      },
+      dna_values: {
+        wants_dna: true,
+        name: 'Paulo',
+        email: 'paulo@example.com'
       }
     }
   end
 
   describe 'PATCH /companies' do
+    before do
+      CompanyUpdate.delete_all
+    end
+
     it 'blocks requests that fails validation' do
-      allow(CompanyUpdate).to receive(:create!) { raise ActiveRecord::RecordInvalid }
-      patch '/companies'
+      params = valid_data.clone
+      %i[partners_values company_values dna_values].each { |k| params.delete k }
+      patch '/companies', params: { company: params }
       expect(response).to have_http_status(:bad_request)
     end
 
     it 'returns the just-created object' do
-      comp_update = valid_data.merge({ id: 'hand-made-id' })
-
-      allow(CompanyUpdate).to receive(:create!).and_return(comp_update)
       patch '/companies', params: { company: valid_data }
-      expect(response.body).to eql({ company_update: comp_update }.to_json)
+      body = JSON.parse response.body
+      expect(body['company_update']['_id']).not_to be_nil
     end
   end
 end
