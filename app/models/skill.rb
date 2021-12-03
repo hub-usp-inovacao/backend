492d06ea49830bdd8a3636d487d0bc30db5bf678
Thiago Guerrero <tgbalera@hotmail.com>
2021-12-03 17:49:48 -0300

    improve skill model
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -24,12 +24,13 @@ class Skill
   validates :name, uniqueness: true
   validates :lattes, :photo, url: true
   validates :phones, phones: true
-  validate :valid_unities?, :valid_keywords?, :valid_bond?, :valid_limit_date?
+  validate :valid_unities?, :valid_campus?, :valid_keywords?, :valid_bond?, :valid_limit_date?,
+           :valid_area?
 
   def valid_limit_date?
     is_valid = limit_date.nil? || limit_date.is_a?(Date)
 
-    errors.add(:limit_date, :invalid) unless is_valid
+    errors.add(:limit_date, 'deve ser uma data válida. Exemplo: dd/mm/yyyy ou N/D') unless is_valid
   end
 
   def valid_bond?
@@ -47,7 +48,7 @@ class Skill
                bond.is_a?(String) &&
                valids.include?(bond)
 
-    errors.add(:bond, :invalid) unless is_valid
+    errors.add(:bond, 'inválido') unless is_valid
   end
 
   def valid_unities?
@@ -55,7 +56,21 @@ class Skill
                unities.is_a?(Array) &&
                unities.all? { |un| all_unities.include? un }
 
-    errors.add(:unities, :invalid) unless is_valid
+    errors.add(:unities, 'inválidas devido a pelo menos uma unidade não válida.') unless is_valid
+  end
+
+  def valid_campus?
+    if campus.nil?
+      errors.add(:campus, ': Não foi possível inferir o campus a partir das unidades')
+    else
+      is_valid =
+        campus.is_a?(String) &&
+        campi.any? do |entry|
+          entry[:name].include? campus
+        end
+
+      errors.add(:campus, 'inválido') unless is_valid
+    end
   end
 
   def valid_keywords?
@@ -63,24 +78,56 @@ class Skill
                keywords.is_a?(Array) &&
                keywords.all? { |kw| kw.is_a?(String) && kw.size.positive? }
 
-    errors.add(:keywords, :invalid) unless is_valid
+    errors.add(:keywords, 'inválidas. Cada palavra-chave deve ser não vazia.') unless is_valid
   end
 
+  def valid_major_area?(major)
+    knowledge_areas.any? { |ka| ka[:name].eql? major }
+  end
+
+  def valid_minor_area?(majors, minor)
+    majors.any? do |major|
+      area = knowledge_areas.detect { |ka| ka[:name].eql? major }
+      area[:subareas].include? minor
+    end
+  end
+
+  def valid_area?
+    if area[:major].nil?
+      errors.add(:area,
+                 ': Não foi possível inferir as grandes áreas a paritr das áreas de conhecimento')
+    else
+      is_valid = area[:major].length.positive? && area[:major].all? do |entry|
+        valid_major_area?(entry)
+      end
+      errors.add(:area, 'inválida. Pelo menos uma das grandes áreas não é válida') unless is_valid
+
+      is_valid &&= area[:major].length.positive? && area[:minor].all? do |entry|
+        valid_minor_area?(area[:major], entry)
+      end
+      unless is_valid
+        errors.add(:area,
+                   'inválida. Pelo menos uma das áreas de conhecimento não é válida')
+      end
+    end
+  end
+
+  # rubocop:disable Metrics/AbcSize
   def self.create_from(row)
-    unis = get_unities(row[5])
+    unis = split_by(row[5], ';')
     skill = new(
       {
         name: row[2],
         email: row[3],
         unities: unis,
-        keywords: kws(row[28]),
+        keywords: split_by(row[28], ';'),
         lattes: row[29],
-        photo: photo_url(row[30]),
+        photo: create_image_url(row[30]),
         skills: split_unless_nd(row[23]),
         services: split_unless_nd(row[24]),
         equipments: split_unless_nd(row[25]),
         phones: split_unless_nd(row[31]),
-        limit_date: row[36],
+        limit_date: get_limit_date(row[36]),
         bond: row[1],
         campus: get_campus(row[6], unis),
         area: get_area(row[26], row[27])
@@ -100,12 +147,13 @@ class Skill
 
     skill
   end
+  # rubocop:enable Metrics/AbcSize
 
   def self.get_area(raw_major, raw_minor)
-    raw_majors = raw_major.split(';')
+    majors = raw_major.split(';')
     minors = raw_minor.split(';')
 
-    if raw_majors.empty?
+    if majors.empty?
       {
         minor: minors,
         major: minors.map { |minor| infer_major(minor) }
@@ -113,7 +161,7 @@ class Skill
     else
       {
         minor: minors,
-        major: raw_majors
+        major: majors
       }
     end
   end
@@ -142,23 +190,25 @@ class Skill
     campus_entry.nil? ? nil : campus_entry[:name]
   end
 
+  def self.get_limit_date(date)
+    return nil if date.eql? 'N/D'
+
+    date
+  end
+
   def self.split_unless_nd(raw)
     return [] if raw.eql? 'N/D'
 
     raw.split(';')
   end
 
-  def self.photo_url(id)
+  def self.create_image_url(id)
     return nil if id.eql?('N/D')
 
     "https://drive.google.com/uc?export=view&id=#{id}"
   end
 
-  def self.kws(raw)
-    raw.split(';')
-  end
-
-  def self.get_unities(raw)
-    raw.split(';')
+  def self.split_by(raw, separator)
+    raw.split(separator)
   end
 end
